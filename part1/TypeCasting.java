6e9f98d6e2f7f4926fc865eaf3a90736c148c7c6
surajgoraicse <surajgoraicse@gmail.com>
2024-10-22 16:38:11 +0530

    starting a new session

--- part1/TypeCasting.java
+++ part1/TypeCasting.java
@@ -1,12 +1,11 @@
+import java.util.*;
+class TypeCasting {
 
-
-class TypeCasting{
     public static void main(String[] args) {
         // int score = 80 , totalScore = 400;
         // float percent = (float) score / totalScore * 100;  // output : 20.0
         // // float percent = score / totalScore * 100;   // output : 0.0
         // System.out.println(percent);
-
 
         // // int num1 = 3434;
         // // float fnum = num1;
@@ -14,38 +13,36 @@ class TypeCasting{
         // float num1  = 232334.343f;
         // double num2 = (long)(double)(int)(long)num1;
         // System.out.println(num2);
-
-
-        int num = 129;
-        byte small = (byte)num;
-        System.out.println(small);
-
-        int a = 888888;
-        int b = 888888;
-        System.out.println(a *b);
-        long c = (long)a * (long)b;
-        System.out.println(c);
-        
+        // int num = 129;
+        // byte small = (byte)num;
+        // System.out.println(small);
+        // int a = 888888;
+        // int b = 888888;
+        // System.out.println(a *b);
+        // long c = (long)a * (long)b;
+        // System.out.println(c);
         // byte a = 12;
         // byte b = 34;
         // byte c = (byte)(a * b) ;
         // System.out.println(c);
-
         // int num = (int)'a';
         // char ch = 'a';
         // byte b = 23;
         // short s = 344;
-
-
         // System.out.println(ch);
-                
-        
-
         // System.out.println(ch * b + s + 3434343874L);
-        
+        // int sum = 0;
+        // for (int i = 1; i <= 10; i++) {
+        //     sum += i;
+        // }
+        // System.out.println(sum / 10.0);
 
-        
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String n = input.nextLine();
+        System.out.println(n);
+
+
 
     }
 }
