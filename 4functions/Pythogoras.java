fc7025ac2851fa0f4810cf405ad14108bb70ed2d
surajgoraicse <surajgoraicse@gmail.com>
2024-10-28 20:54:38 +0530

    factorial

---
+++ 4functions/Pythogoras.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Pythogoras {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter three numbers");
+        int m = input.nextInt();
+        int n = input.nextInt();
+        int o = input.nextInt();
+        boolean ans = check(m, n, o);
+        System.out.println(ans);
+    }
+
+    static boolean  check(int a , int b , int c){
+        int largest = Math.max(c,Math.max(a, b));
+        int smallest = Math.min(c,Math.min(a, b));
+        int middle = a + b + c - largest - smallest;
+
+        return (Math.sqrt(smallest * smallest + middle * middle) == largest);
+        
+
+    }
+}
